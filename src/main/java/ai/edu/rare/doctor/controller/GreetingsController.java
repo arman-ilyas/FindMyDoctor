84e2b2ae27622dfbff3cf3b2798390b6488e59ff
ArmanGT <arman987@gmail.com>
2020-04-01 23:00:39 +0800

    add test controller for spring security

---
+++ src/main/java/ai/edu/rare/doctor/controller/GreetingsController.java
@@ -0,0 +1,12 @@
+package ai.edu.rare.doctor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+  @GetMapping("/morning")
+  public String getGreetings() {
+    return "Good Morning";
+  }
+}
